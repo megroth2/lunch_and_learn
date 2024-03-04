013b653e4c7e23fec91d51d8977e0ac3c8e0627f
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-03 19:20:01 -0700

    fix favorites model and spec to remove recipes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,5 @@
 class Favorite < ApplicationRecord
-  validates :recipe_id, presence: true, numericality: true
   validates :user_id, presence: true, numericality: true
   
-  belongs_to :recipe
   belongs_to :user
 end
