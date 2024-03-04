603b6a4a231d71b3a20d18730fabd73e0bac8e75
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-03 23:12:48 -0700

    refine recipe_facade_spec

--- spec/facades/recipe_facade_spec.rb
+++ spec/facades/recipe_facade_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe "RecipeFacade" do
     country = "thailand" 
 
     recipes = RecipeFacade.get_recipes(country)
+    recipe = recipes.first
 
     expect(recipes).to be_an(Array)
-    expect(recipes.first).to be_a(RecipePoro)
-    expect(recipes.first.title).to be_a(String)
-    expect(recipes.first.url).to be_a(String)
-    # binding.pry
-    # expect(recipes.first.country).to be_a(String)
-    # expect(recipes.first.country).to eq("thailand")
-    expect(recipes.first.image).to be_a(String)
+    expect(recipe).to be_a(RecipePoro)
+    expect(recipe.title).to be_a(String)
+    expect(recipe.url).to be_a(String)
+    expect(recipe.image).to be_a(String)
   end
 end
