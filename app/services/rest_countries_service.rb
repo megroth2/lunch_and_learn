df7680e581e097a32bc0e5eecf5beacde91e029e
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 09:00:15 -0700

    remove comments

--- app/services/rest_countries_service.rb
+++ app/services/rest_countries_service.rb
@@ -1,27 +1,6 @@
-# class RestCountriesService
-#   def conn
-#     Faraday.new(url: 'https://restcountries.com/v3.1/all') do |faraday|
-#       faraday.params['fields'] = "name,capital,latlng"
-#     end
-#   end
-
-#   def get_url(url)
-#     response = conn.get(url) 
-#     JSON.parse(response.body, symbolize_names: true)
-#   end
-
-#   def get_country(country)
-#     get_url("/")
-#   end
-
-#   def get_countries
-#     get_url("")
-#   end
-# end
-
 class RestCountriesService
   def conn
-    Faraday.new(url: 'https://restcountries.com/') do |faraday| # needs all or name to work
+    Faraday.new(url: 'https://restcountries.com/') do |faraday|
       faraday.params['fields'] = "name,capital,latlng"
     end
   end
