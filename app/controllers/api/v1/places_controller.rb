e5c15bc9c88e4ee868053f3b31bcc852ce7facb9
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 10:41:01 -0700

    complete refactor of first connection (facade and poro, no service)

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -3,23 +3,9 @@ class Api::V1::PlacesController < ApplicationController
   def index
     country = CountryFacade.get_country(params[:country])
 
-    # conn_1 = Faraday.new(url: "https://restcountries.com/v3.1/name/#{params[:country]}") do |faraday|
-    #   faraday.params['fields'] = "name,capital,latlng"
-    # end
-
-    # response_1 = conn_1.get
-    
-    # json_1 = JSON.parse(response_1.body, symbolize_names: true) 
-
-    # country = json_1.first
-
-    ##############################
-    lat = country[:latlng].first
-    lng = country[:latlng].second
-
     conn = Faraday.new(url: "https://api.geoapify.com/v2/places") do |faraday|
       faraday.params['categories'] = "tourism"
-      faraday.params['bias'] = "proximity:#{lat},#{lng}"
+      faraday.params['bias'] = "proximity:#{country.lat},#{country.lng}"
       faraday.params['limit'] = "10"
       faraday.params['apiKey'] = Rails.application.credentials.geoapify[:key]
     end
--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,7 +1,7 @@
 class CountryFacade
 
   def self.get_country(country)
-    conn = Faraday.new(url: "https://restcountries.com/v3.1/name/#{params[:country]}") do |faraday|
+    conn = Faraday.new(url: "https://restcountries.com/v3.1/name/#{country}") do |faraday|
       faraday.params['fields'] = "name,capital,latlng"
     end
 
@@ -9,7 +9,6 @@ class CountryFacade
     
     json = JSON.parse(response.body, symbolize_names: true) 
 
-    country = json.first
-    binding.pry
+    country = CountryPoro.new(json.first)
   end
 end
