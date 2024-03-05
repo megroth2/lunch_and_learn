e4de396653eb96e32ab270a886377f42b7736bf6
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-04 18:53:50 -0700

    add route for learning resources

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/recipes', to: 'recipes#index'
+      get '/learning_resources', to: 'learning_resources#index'
 
       # resources :users do
 	    #   resources :favorites, only: [:index, :create]
