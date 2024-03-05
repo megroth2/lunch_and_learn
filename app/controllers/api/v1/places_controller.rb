7b585711bfe60ea3d527bc4fa5e351c4fd353d2e
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 10:11:35 -0700

    wip

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::PlacesController < ApplicationController
 
-  def index(lat, lng) # how do i pass these in? they aren't in params
+  def index
+
+    conn_1 = Faraday.new(url: "https://restcountries.com/v3.1/name/#{params[:country]}") do |faraday|
+      faraday.params['fields'] = "name,capital,latlng"
+    end
+
+    response_1 = conn_1.get
+    
+    json_1 = JSON.parse(response_1.body, symbolize_names: true) 
+
+    country = json_1.first
+
+    ##############################
+    lat = country[:latlng].first
+    lng = country[:latlng].second
+
     conn = Faraday.new(url: "https://api.geoapify.com/v2/places") do |faraday|
       faraday.params['categories'] = "tourism"
       faraday.params['bias'] = "proximity:#{lat},#{lng}"
@@ -13,6 +28,12 @@ class Api::V1::PlacesController < ApplicationController
     json = JSON.parse(response.body, symbolize_names: true)
 
     places = json[:features]
+    binding.pry
+
+    #############################
+
+    # country = RestCountriesService.new.get_country(params[:country])
+    # places = GeoapifyService.new.get_places(lat, lng)
     render json: PlaceSerializer.format_places(places, params[:country])
   end
 end
