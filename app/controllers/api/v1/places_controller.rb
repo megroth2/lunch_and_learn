0d466c3abc7d5c63195896ed0eb893b6a6c9f3d2
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 09:53:23 -0700

    add places connection to index action

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::PlacesController < ApplicationController
 
-  def index
-    
+  def index(lat, lng) # how do i pass these in? they aren't in params
+    conn = Faraday.new(url: "https://api.geoapify.com/v2/places") do |faraday|
+      faraday.params['categories'] = "tourism"
+      faraday.params['bias'] = "proximity:#{lat},#{lng}"
+      faraday.params['limit'] = "10"
+      faraday.params['apiKey'] = "Rails.application.credentials.geoapify[:key]"
+    end
+
+    response = conn.get
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    places = json[:features]
+    render json: PlaceSerializer.format_places(places, params[:country])
   end
 end
