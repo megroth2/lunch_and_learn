d4e1a3c82b005b52472d519bcc3a3d33a163f457
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 11:48:46 -0700

    remove comments

--- app/facades/place_facade.rb
+++ app/facades/place_facade.rb
@@ -1,13 +1,6 @@
 class PlaceFacade
 
   def self.get_places(country_poro)
-    # service = EdemamService.new
-    # json_response = service.get_recipes(country)
-
-    # @recipes = json_response.map do |recipe|
-    #   RecipePoro.new(recipe, country)
-    # end
-
     service = GeoapifyService.new
     json_response = service.get_places(country_poro)
 
