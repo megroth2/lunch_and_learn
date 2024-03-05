4f5bf1153342bc1fe234eca80927b4288f31ef78
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 09:20:23 -0700

    request spec

---
+++ spec/requests/api/v1/index_places_by_country_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe "Api::V1::Places" do
+  describe "[happy paths]" do
+    it "sends a list of places for a particular country", :vcr do
+      get "/api/v1/tourist_sites?country=France"
+
+      expect(response).to have_http_status(:success)
+
+      places = JSON.parse(response.body, symbolize_names:true)[:data]
+
+      expect(places.count).to eq(10)
+
+      places.each do |place|
+        expect(place).to have_key(:id)
+        expect(place[:id]).to eq(nil)
+
+        expect(place).to have_key(:type)
+        expect(place[:type]).to eq("tourist_site")
+
+        expect(place[:attributes]).to have_key(:name)
+        expect(place[:attributes][:name]).to be_a(String)
+
+        expect(place[:attributes]).to have_key(:address)
+        expect(place[:attributes][:address]).to be_a(String)
+
+        expect(place[:attributes]).to have_key(:place_id)
+        expect(place[:attributes][:place_id]).to be_a(String)
+      end
+    end
+  end
+
+  describe "[sad paths]" do
+    it "will gracefully handle if there are no results for a given country" do
+      # Ecuador, Uruguay
+    end
+  end
+end
