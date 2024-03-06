e8d8bb9b3e4b8b66812d131d52622f2acaa5d03d
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 17:32:33 -0700

    clean up recipe poro

--- app/poros/recipe_poro.rb
+++ app/poros/recipe_poro.rb
@@ -6,24 +6,8 @@ class RecipePoro
               :image
 
   def initialize(data, country)
-    # binding.pry
-    # @title = data[:_links][:self][:title]
-    # @url = data[:recipe][:url]
-    # @country = country
-    # @image = data[:hits][0][:recipe][:image]
-
-    # this works for recipe_poro_spec.rb
-    # binding.pry
     @title = data[:recipe][:label]
     @image = data[:recipe][:image]
     @url = data[:recipe][:url]
   end
-
-  # def random_select_country
-  #   binding.pry
-  #   if !params[:country]
-  #     # use the countries API to randomly select
-  #   else
-  #   end
-  # end
 end
