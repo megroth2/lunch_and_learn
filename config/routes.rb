1be9b8405635abcd70ac95fa85bdf3c5cd169410
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-03-05 22:45:47 -0700

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/recipes', to: 'recipes#index'
       get '/learning_resources', to: 'learning_resources#index'
+      post '/sessions', to: 'sessions#create'
       resources :users, only: [:create]
       
       # resources :users do
